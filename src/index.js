583517b9fe120c843091bb3f15524438ca446b91
Gagan <bhatiagagan24@gmail.com>
2021-07-04 22:41:54 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Button} from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// import './index.css';
+
+
+<meta
+  name="viewport"
+  content="minimum-scale=1, initial-scale=1, width=device-width"
+/>
+
+function headsOrTails() {
+  var temp1 = Math.random() > 0.5 ? 'heads' : 'tails';
+  return temp1;
+}
+
+
+var jsx1 = 
+(
+<Box textAlign='center'>
+<div>
+<p>Press the button below to Toss a Coin</p>
+<Button variant="contained" color="primary" onClick={newJsx}>Toss the Coin</Button>
+</div>
+</Box>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function reset() {
+jsx1 = 
+(
+<Box textAlign='center'>
+<div>
+<p>Press the button below to Toss a Coin</p>
+<Button variant="contained" color="primary" onClick={newJsx}>Toss the Coin</Button>
+</div>
+</Box>
+);
+ReactDOM.render(jsx1, document.getElementById('root'));
+}
+
+var heads = 0;
+var tails = 0;
+
+function newJsx(e) {
+  var newC = headsOrTails();
+  if(newC === 'heads') {
+    heads += 1;
+  } else {
+    tails += 1;
+  }
+  jsx1 = (
+  <Box textAlign='center'>
+  <div>
+  <p>You got {newC}</p>
+  <p>Heads : -  {heads}</p>
+  <p>Tails : -  {tails}</p>
+  <Button variant="outlined" color="primary" onClick={newJsx}>Toss the coin again</Button> <br /><br />
+  <Button variant="outlined" color="secondary" onClick={reset}>Reset the Score</Button>
+  </div>
+  </Box>
+  );
+ReactDOM.render(jsx1, document.getElementById('root'));
+}
+
+ReactDOM.render(jsx1, document.getElementById('root'));
